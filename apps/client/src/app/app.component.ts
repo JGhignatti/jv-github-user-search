1ad7a0f51ec7f14f44c3052667ed97383b0c1e14
JGhignatti <joao.ghignatti@inf.ufrgs.br>
2021-07-28 18:33:04 -0300

    Add base style and behavior for search field connected to route

--- apps/client/src/app/app.component.ts
+++ apps/client/src/app/app.component.ts
@@ -1,9 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
 
+import { UntilDestroy, untilDestroyed } from '@ngneat/until-destroy';
+import { debounceTime, distinctUntilChanged } from 'rxjs/operators';
+
+@UntilDestroy()
 @Component({
   selector: 'jv-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
+  searchControl = new FormControl('');
+
+  constructor(private router: Router,
+              private activatedRoute: ActivatedRoute) {
+  }
+
+  ngOnInit() {
+    this.searchControl.valueChanges
+      .pipe(
+        untilDestroyed(this),
+        debounceTime(800),
+        distinctUntilChanged(),
+      )
+      .subscribe(value => {
+        this.router.navigate([], {
+          relativeTo: this.activatedRoute,
+          queryParams: { s: value || null },
+        });
+      });
+  }
 }
--- apps/client/src/app/app.module.ts
+++ apps/client/src/app/app.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { ReactiveFormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 
 @NgModule({
-  imports: [BrowserModule],
+  imports: [
+    BrowserModule,
+    ReactiveFormsModule,
+    RouterModule.forRoot([]),
+  ],
   declarations: [AppComponent],
   bootstrap: [AppComponent],
 })
